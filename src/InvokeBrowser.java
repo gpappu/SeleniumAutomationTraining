9a14b6086cd5df057424bf7432049d51402a14e9
chari <charismatic.g@gmail.com>
2023-03-07 12:28:00 -0500

    added new code

---
+++ src/InvokeBrowser.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class InvokeBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://www.udemy.com/");
+
+	}
+
+}
